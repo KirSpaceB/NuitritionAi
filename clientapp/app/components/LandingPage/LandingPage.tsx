c690f978406b8038bf9d8af7a79b83540fdf9733
KirkSpaceB <productivekirk@gmail.com>
2023-08-25 14:58:37 -0700

    feat:"added LandingPage text"

---
+++ clientapp/app/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function LandingPage() {
+  return (
+    <div>
+      <div id='LP_Header_Text' className='flex flex-col ml-10 mt-10 sm'>
+        <h1 className='sm:text-6xl'>Landing Page</h1>
+        <h3 className='sm:text-4xl'>living made easy!</h3>
+      </div>
+
+      <div>
+        <p>
+          Unlock the future of nutrition with our AI-powered platform, tailored to serve your unique dietary needs and goals. Experience personalized meal planning, real-time nutrient tracking, and science-backed recommendations, all in one place. Elevate your health journey today with the precision and intelligence only our AI nutrition helper can offer."
+        </p>
+      </div>
+    </div>
+  )
+}
