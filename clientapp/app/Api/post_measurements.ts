99018256d6d939cf8175bff1c5e54b8a3144f4aa
KirSpaceB <productivekirk@gmail.com>
2023-09-02 21:45:18 -0700

    'feat:flask api connected'

--- clientapp/app/Api/post_measurements.ts
+++ clientapp/app/Api/post_measurements.ts
@@ -1,14 +1,17 @@
 import { IMeasurements } from "../components/shared/context/MeasurementsData";
 
 export async function sendMeasurementsToApi(measurements: IMeasurements) {
-  console.log('measuyrements in sendMeasurementsToAPi',measurements)
-  const request = await fetch('http://localhost:5000/apifromflask', {
+  
+  if (measurements == null || measurements == undefined) {
+    console.error('measurements is undefined: This code is coming from line 6 of post_measurements.ts')
+  }
+  const request = await fetch('http://localhost:5000/connected_api', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(measurements),
   });
-
-  return request.json();
+  const response = await request.json();
+  console.log(response)
 }
